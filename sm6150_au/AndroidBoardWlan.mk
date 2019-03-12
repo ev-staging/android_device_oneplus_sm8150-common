ec5ee920c83220da55aeb7b2392750618319ca0c
Yu Wang <yyuwang@codeaurora.org>
2019-03-12 12:48:30 +0800

    sm6150_au: add support for multiple wlan drivers
    Add wlan related files according to 'TARGET_WLAN_CHIP'.
    
    Change-Id: If0b3a6912ffcb962cfab6c2e499100b626c9f018

---
+++ sm6150_au/AndroidBoardWlan.mk
@@ -0,0 +1,41 @@
+LOCAL_WLAN_SRC := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := $(LOCAL_WLAN_SRC)/wpa_supplicant_overlay.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/wifi
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := $(LOCAL_WLAN_SRC)/p2p_supplicant_overlay.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/wifi
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := $(LOCAL_WLAN_SRC)/hostapd.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_SRC_FILES    := hostapd.conf
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := $(LOCAL_WLAN_SRC)/hostapd.accept
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_SRC_FILES    := hostapd.accept
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := $(LOCAL_WLAN_SRC)/hostapd.deny
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_SRC_FILES    := hostapd.deny
+include $(BUILD_PREBUILT)
