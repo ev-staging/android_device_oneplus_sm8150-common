karin722/device_oneplus_guacamoleb

sm8150-common: Use Soong namespace to avoid device build conflicts

BoardConfigCommon.mk
@@ -6,6 +6,7 @@
 
 BOARD_VENDOR := oneplus
 DEVICE_PATH_COMMON := device/oneplus/sm8150-common
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH_COMMON)
 
 # Architecture
 TARGET_ARCH := arm64

ev-staging/android_device_oneplus_sm8150-common

Change-Id: I3e8252596eb9dec58c6861e17ba5decbeb0b8e34

BoardConfigCommon.mk
@@ -15,6 +15,7 @@
 #
 
 PLATFORM_PATH := device/oneplus/sm8150-common
+PRODUCT_SOONG_NAMESPACES += $(PLATFORM_PATH)
 
 # Architecture
 TARGET_ARCH := arm64